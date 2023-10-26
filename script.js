74750d112bc5c593fad952345bcbe284f5f97d3b
Veronica0014 <119103207+Veronica0014@users.noreply.github.com>
2023-10-26 09:50:36 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const input = document.querySelector('input')
+const answer2 = document.querySelector('.answer')
+const error = document.querySelector('.error')
+const answers = [
+	'yes 👍',
+	'no👎',
+	'maybe 🤞',
+	'depends',
+	'Without a doubt',
+	'Cannot predict now',
+	'My reply is no',
+	'As I see it, yes ✌️',
+]
+const btnRemove = document.querySelector('.btn-remove ')
+
+const checkQuestion = () => {
+	if (input.value.includes('?')) {
+		randomAnswer()
+
+		error.textContent = ''
+	} else if (input.value === '') {
+		error.textContent = ' *Enter your question'
+	} else {
+		error.textContent = '*Ask me again using a question mark'
+		answer2.innerHTML = ''
+	}
+}
+
+const randomAnswer = () => {
+	const number = Math.floor(Math.random() * 5)
+
+	answer2.innerHTML = `<span>Answer: </span>${answers[number]}`
+	error.textContent = ''
+}
+
+const handleKeyDown = e => {
+	if (e.key === 'Enter') {
+		checkQuestion()
+	}
+}
+
+const clearAll = () => {
+	answer2.textContent = ''
+	input.value = ''
+	error.textContent = ''
+}
+
+input.addEventListener('keydown', handleKeyDown)
+btnRemove.addEventListener('click', clearAll)
